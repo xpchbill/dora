15d2d18981a93f8991c7aeb4935de5e19f080469
bill.xiong <xpchbill8@gmail.com>
2020-06-08 20:07:52 +0800

    Run eject

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -4,8 +4,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
